a91a402f1788623572792b6eb0f9983aebda350b
Samuel Pereira <gmr.quest.10@gmail.com>
2023-03-01 09:52:19 -0300

    refactor: transform login function to arrow function

--- src/utils/fetchLogin.ts
+++ src/utils/fetchLogin.ts
@@ -4,6 +4,6 @@ import { IUserCredentials } from '../interfaces/UserCredentials';
 const LOGIN_ENDPOINT =
   'https://contacts-api.prd.parceirodaconstrucao.com.br/auth/login';
 
-export async function login({ email, password }: IUserCredentials) {
+export const login = async ({ email, password }: IUserCredentials) => {
   return axios.post(LOGIN_ENDPOINT, { email, password }).then(res => res.data);
-}
+};
