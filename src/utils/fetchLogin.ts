be04e0c6abf38f986872bf895d13a3d35200be24
Samuel Pereira <gmr.quest.10@gmail.com>
2023-03-01 09:49:40 -0300

    refactor: break line

--- src/utils/fetchLogin.ts
+++ src/utils/fetchLogin.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { IUserCredentials } from '../interfaces/UserCredentials';
 
-const LOGIN_ENDPOINT = 'https://contacts-api.prd.parceirodaconstrucao.com.br/auth/login';
+const LOGIN_ENDPOINT =
+  'https://contacts-api.prd.parceirodaconstrucao.com.br/auth/login';
 
 export async function login({ email, password }: IUserCredentials) {
   return axios.post(LOGIN_ENDPOINT, { email, password }).then(res => res.data);
